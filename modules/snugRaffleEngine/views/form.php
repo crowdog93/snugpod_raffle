e52d383cd4d2b757ff2df8e3f613ff842d07ebee
crowdog93 <collin.d.deputee@protonmail.com>
2021-12-13 10:48:27 -0700

    Fixed form

--- modules/snugRaffleEngine/views/form.php
+++ modules/snugRaffleEngine/views/form.php
@@ -10,6 +10,10 @@
       <small id="gamerTagHelp" class="form-text text-muted">Ensure you enter it correctly so they can be located.</small>
     </div>
     <div class="form-group">
+      <label for="raffle_id">Raffle</label>
+      <input type="text" class="form-control raffle_main" id="raffle_id" aria-describedby="raffle" placeholder="Raffle">
+    </div>
+    <div class="form-group">
       <label for="total_enteries">Entries</label>
       <input type="number" class="form-control" id="total_enteries" placeholder="Entries">
     </div>
@@ -17,23 +21,20 @@
 
    </div>
 </div>
-<div class="raffle_enteries"
+<div class="raffle_enteries">
   <table id="entry_table" class="table table-striped table-bordered table-hover">
     <thead>
       <tr id="raffle_header_row">
         <th scope="col">Gamer Tag</th>
+        <th scope="col">Raffle</th>
         <th scope="col">Enteries</th>
       </tr>
     </thead>
     <tbody>
-      <tr>
-        <td id="gamerTagTable"></td>
-        <td id="enteriesTable"></td>
-      </tr>
+
     </tbody>
   </table>
 </div>
-<?php echo ajax_modal_template_footer(TEXT_SEND) ?>
 
 </form>
 
@@ -42,16 +43,17 @@
 
 document.getElementById("submit_entry").onclick=function()
 {
-    document.getElementById("entry_table").style.display="block";
-
     var table = document.getElementById("entry_table");
+    console.log(table)
     var row = table.insertRow(-1);
     var gamerTag = row.insertCell(0);
-    var enteries = row.insertCell(1);
+    var total_enteries = row.insertCell(1);
+    var raffle = row.insertCell(2);
     gamerTag.innerHTML = document.getElementById("gamerTag").value;
     total_enteries.innerHTML = document.getElementById("total_enteries").value;
+    raffle.innerHTML = document.getElementById("raffle_id").value;
 
-    return false;
+    return console.log('Added Raffle');
 }
 
 </script>
