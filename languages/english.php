195b9b248cccc5c4f05f653d62d8482fbf1077d8
crowdog93 <collin.d.deputee@protonmail.com>
2021-12-13 11:43:18 -0700

    Raffle updates

--- languages/english.php
+++ languages/english.php
@@ -1,4 +1,4 @@
 <?php
 
-define('TEXT_PLUGIN_SNUG_RAFFLE','Welcome to the Snugpod Raffle Engine!');
+define('TEXT_PLUGIN_SNUG_RAFFLE','Welcome to the Snugpod Raffle Engine! This module is under development and is not functional');
 define('TEXT_PLUGIN_SNUG_FORM_HEADER','Add Snugs to Raffle');
--- menu.php
+++ menu.php
@@ -4,5 +4,5 @@
 $access = array("4", "12", "16","0");
 if(in_array($app_user['group_id'], $access))
 {
-  $app_plugin_menu['menu'][] = array('title'=>'Snugpod Raffle','url'=>url_for('raffle/snugRaffleEngine/index'),'class'=>'fa-ticket');
+  $app_plugin_menu['menu'][] = array('title'=>'Snugpod Raffle Engine','url'=>url_for('raffle/snugRaffleEngine/index'),'class'=>'fa-ticket');
 }
